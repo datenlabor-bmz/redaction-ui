ee2449e8be9aee0ae8a27599f6204631c578da97
David Pomerenke <46022183+davidpomerenke@users.noreply.github.com>
2025-12-29 17:12:54 +0100

    add types

---
+++ src/mupdf.d.ts
@@ -0,0 +1,6 @@
+// Type declarations for mupdf/mupdfjs
+declare module 'mupdf/mupdfjs' {
+  export type Quad = [number, number, number, number, number, number, number, number]
+  export type Rect = [number, number, number, number]
+}
+
